1371dc386a64ea4e4308fbf98eee0fffdaf3b46e
ariel.morelli <ariel.morelli@celus.io>
2023-07-02 15:25:45 +0200

    feat: suport aarch64

--- dev/generate-bin.sh
+++ dev/generate-bin.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-ARCHS=(amd64 arm64 ppc64le ppc64 s390x)
-
+ARCHS=(amd64 arm64)
 
 # install dependencies
 go mod download
@@ -15,4 +14,3 @@ for target_arch in ${ARCHS[@]}
 do
 	env GOOS=linux GOARCH=${target_arch} go build -ldflags "-w" -o bin/servus-api-${target_arch}
 done
-
